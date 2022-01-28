c105b0509646c38fb1cc956257d346b97130b214
randy <baumer.randy@gmail.com>
2022-01-27 19:50:22 -0500

    build

---
+++ client/build/precache-manifest.1912245a22c02d8961a26556ffaa490d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f082c28b33c367077af171809901cabe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81e78a55bb542d85c16c",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6dc8f9912d32288c42af",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "81e78a55bb542d85c16c",
+    "url": "/static/js/2.f4b7ce9c.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.f4b7ce9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc8f9912d32288c42af",
+    "url": "/static/js/main.d32b0f22.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
