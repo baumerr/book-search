eb738d2a63ec3cda62b9b4d2360b2d6dcb07ccbb
randy <baumer.randy@gmail.com>
2022-01-27 19:58:24 -0500

    added server

--- server/server.js
+++ server/server.js
@@ -27,9 +27,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
